2c42be9795196e50f03576673f0ec369e277197a
Ángelcoy <angelalbertogarcia1@gmail.com>
2026-09-03 22:23:04 +0000

    added limits for the size of the grid
    just added a provisional limiter to the set of grids on the prompt. later i will upgrade it

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ const container = document.querySelector(".container")
 
 
 function divGenerator (number) {
-    const size = 100 / number;
     
-    for( let i = 0; i < number*number; i++) {
-       const cell = document.createElement("div");
-        cell.classList.add("grid");
-        cell.style.width = size + "%"
-        cell.style.height = size + "%"
-        container.appendChild(cell);
-        
+    if (number < 0 || number > 64) {
+        return alert("limit exceded")
     }
-    
+    else {
+      
+      const size = 100 / number;
+
+      for( let i = 0; i < number*number; i++) {
+          const cell = document.createElement("div");
+           cell.classList.add("grid");
+           cell.style.width = size + "%"
+           cell.style.height = size + "%"
+           container.appendChild(cell);
+         
+     }
+   }
 }
 
-divGenerator(20)
+divGenerator(10)
